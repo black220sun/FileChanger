7aa8199f4800047b4f1cff4e3aa5592bae0d83dc
black220sun <shulepov.vlad@gmail.com>
2018-01-05 13:48:00 +0200

    Reworked single file actions. Added tests for all actions

--- src/main/java/filechanger/FileChanger.kt
+++ src/main/java/filechanger/FileChanger.kt
@@ -9,12 +9,15 @@ class FileChanger {
     private val translationPath = "translate.csv"
     private val replacementPath = "replace.csv"
 
-    fun load() {
-        init()
-        init(replacement, replacementPath)
+    fun loadReplacement(path: String = replacementPath) {
+        load(replacement, path)
     }
 
-    fun init(provider: HashMap<String, String> = translation, path: String = translationPath) {
+    fun loadTranslation(path: String = translationPath) {
+        load(translation, path)
+    }
+
+    private fun load(provider: HashMap<String, String>, path: String) {
         val file = File(path)
         if (!file.exists())
             return
@@ -30,7 +33,11 @@ class FileChanger {
         return rename(file, translation)
     }
 
-    fun rename(file: File, provider: HashMap<String, String> = replacement): File {
+    fun rename(file: File): File {
+        return rename(file, replacement)
+    }
+
+    private fun rename(file: File, provider: HashMap<String, String>): File {
         var name = file.nameWithoutExtension
         provider.forEach{from, to -> name = name.replace(from, to)}
         val newName = if (file.extension.isEmpty()) name else "$name.${file.extension}"
@@ -39,7 +46,7 @@ class FileChanger {
         return new
     }
     fun move(file: File, path: String): File {
-        if (!path.matches("(/[^\\s/]+)*".toRegex()))
+        if (!path.matches("([^/]+)?(/[^/]+)*".toRegex()))
             return file
         File(path).mkdirs()
         val new = File("$path/${file.name}")
@@ -47,7 +54,7 @@ class FileChanger {
         return new
     }
     fun moveByName(file: File, path: String = "", delimiter: String = "_-_"): File {
-        if (!path.matches("(/[^\\s/]+)*".toRegex()))
+        if (!path.matches("([^/]+)?(/[^/]+)*".toRegex()))
             return file
         val dir = file.name.split(delimiter)[0]
         val destination = if (path.isEmpty()) dir else "$path/$dir"
@@ -65,7 +72,7 @@ class FileChanger {
         add(from, to, replacement)
     }
 
-    fun add(from: String, to: String, provider: HashMap<String, String> = replacement) {
+    private fun add(from: String, to: String, provider: HashMap<String, String>) {
         provider.put(from, to)
     }
 
--- src/test/java/FileChangerTest.kt
+++ src/test/java/FileChangerTest.kt
@@ -1,6 +1,7 @@
 import filechanger.FileChanger
 import org.junit.Test
 import java.io.File
+import kotlin.test.assertEquals
 
 class FileChangerTest {
     @Test
@@ -8,13 +9,13 @@ class FileChangerTest {
         val file = File("testoVaJa prograMMa")
         file.createNewFile()
         val changer = FileChanger()
-        changer.load()
+        changer.loadTranslation()
 
         val new = changer.translate(file)
 
-        assert(new.exists())
-        assert(new.name == "тестоВаЯ програММа")
-        assert(!file.exists())
+        assertEquals(true, new.exists())
+        assertEquals("тестоВаЯ програММа", new.name)
+        assertEquals(false, file.exists())
 
         new.delete()
     }
@@ -23,14 +24,82 @@ class FileChangerTest {
         val file = File("Jupiter - Devushka po gorodu shagaet bosikom.mp3")
         file.createNewFile()
         val changer = FileChanger()
-        changer.load()
+        changer.loadTranslation()
 
         val new = changer.translate(file)
 
-        assert(new.exists())
-        assert(new.name == "Юпитер - Девушка по городу шагает босиком.mp3")
-        assert(!file.exists())
+        assertEquals(true, new.exists())
+        assertEquals("Юпитер - Девушка по городу шагает босиком.mp3", new.name)
+        assertEquals(false, file.exists())
 
         new.delete()
+    }
+    @Test
+    fun renameTest() {
+        val file = File("AlisA_-_Rodina_(zf.fm).mp3")
+        file.createNewFile()
+        val changer = FileChanger()
+        changer.addReplacement("_", " ")
+        changer.addReplacement(" (zf.fm)", "")
+
+        val new = changer.rename(file)
+
+        assertEquals(true, new.exists())
+        assertEquals("AlisA - Rodina.mp3", new.name)
+        assertEquals(false, file.exists())
+
+        new.delete()
+    }
+    @Test
+    fun moveTest() {
+        val file = File("AlisA_-_Rodina.mp3")
+        file.createNewFile()
+        val changer = FileChanger()
+        val dir = File("AlisA")
+        dir.mkdir()
+
+        val new = changer.move(file, "AlisA")
+
+        assertEquals(true, new.exists())
+        assertEquals(false, file.exists())
+        assertEquals("AlisA", new.parentFile.name)
+        assertEquals(dir.absolutePath, new.parentFile.absolutePath)
+
+        new.delete()
+        dir.delete()
+    }
+    @Test
+    fun moveAbsolutePathTest() {
+        val file = File("AlisA_-_Rodina.mp3")
+        file.createNewFile()
+        val changer = FileChanger()
+        val dir = File("AlisA")
+        dir.mkdir()
+
+        val new = changer.move(file, "/home/blacksun/IdeaProjects/FileChanger/AlisA")
+
+        assertEquals(true, new.exists())
+        assertEquals(false, file.exists())
+        assertEquals("AlisA", new.parentFile.name)
+        assertEquals(dir.absolutePath, new.parentFile.absolutePath)
+
+        new.delete()
+        dir.delete()
+    }
+    @Test
+    fun moveByNameTest() {
+        val file = File("AlisA_-_Rodina.mp3")
+        file.createNewFile()
+        val changer = FileChanger()
+
+        val new = changer.moveByName(file, delimiter = "_-_")
+
+        assertEquals(true, new.exists())
+        assertEquals(false, file.exists())
+        assertEquals(true, File("AlisA").isDirectory)
+        assertEquals("AlisA", new.parentFile.name)
+
+        new.delete()
+        File("AlisA").delete()
     }
 }
